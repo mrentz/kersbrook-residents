89c95f77082837564c4faf60a65f3948aaf7cb60
Matthew <daniel@codefire.com.au>
2011-11-03 23:22:56 +1030

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KersbrookResidents::Application.config.secret_token = 'ca3abf6aa67f47251ef6de2bf1f38f1d2e58a956d15ecc6ee7b595053c8b5223cadbf0a6f22918fc318eb2ba884aac4f5c7a23f4706f8f09c2b27d457ebecdc8'
